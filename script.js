2b5ae89480d310284f8e6e029d323d03c2c55dab
lakshay <126890120+lakshay029@users.noreply.github.com>
2026-04-23 13:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+let grid = document.querySelector('.container');
+
+for (let i = 0; i < 800; i++) {
+  let sq = document.createElement('div');
+  sq.className = 'square';
+
+	sq.addEventListener('mouseover',()=>{
+		const color = `rgb(${Math.random()*256}, ${Math.random()*256}, ${Math.random()*256})`;
+		sq.style.backgroundColor=color;
+
+	
+	})
+	sq.addEventListener('mouseout',()=>{
+		sq.style.backgroundColor='rgb(29,29,29)';
+		sq.style.transition="background-color 1s";
+		
+	})
+  grid.appendChild(sq); 
+}
